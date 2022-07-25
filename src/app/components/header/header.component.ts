058a8690edb7aa6e09b447aba537a82780c59b93
Mihai Mirzac <mihaimarzac@gmail.com>
2022-07-25 15:26:26 +0300

    Added header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CountryService } from 'src/app/services/country.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+
+  @Output() toggledTheme: boolean = false;
+  @Output() buttonClicked: EventEmitter<any> = new EventEmitter();
+
+  countriesData:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(window.localStorage.getItem('toggledTheme')){
+      this.toggledTheme = true;
+    }
+
+  }
+
+
+  toggleTheme(): void{
+
+    if(window.localStorage.getItem('toggledTheme')){
+      this.toggledTheme = !this.toggledTheme;
+      window.localStorage.removeItem('toggledTheme');
+     // window.localStorage.setItem('toggledTheme', `${this.toggledTheme}`);
+      this.buttonClicked.emit(this.toggledTheme);
+    } else {
+      this.toggledTheme = !this.toggledTheme;
+      window.localStorage.setItem('toggledTheme', `${this.toggledTheme}`);
+      this.buttonClicked.emit(this.toggledTheme);
+    }
+    //this.toggledTheme = !this.toggledTheme;
+    //this.buttonClicked.emit(this.toggledTheme);
+
+  
+  }
+
+}
