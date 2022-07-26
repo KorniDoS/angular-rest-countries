221154d818271aabe909a6ef51d04b7fab504a6a
Mihai Mirzac <mihaimarzac@gmail.com>
2022-07-26 16:04:35 +0300

    Removed console.logs

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -21,57 +21,47 @@ export class CountriesComponent implements OnInit, OnDestroy {
   filteredCountries: any[] = [];
 
 
-  countryDataSub?:Subscription;
+  countryDataSub?: Subscription;
 
   constructor(private countryService: CountryService) { }
 
 
   ngOnInit(): void {
     this.countryDataSub = this.countryService.getAllCountries().subscribe(
-      res=>{
+      res => {
         this.countriesData = res;
         this.countriesDataCopy = this.countriesData;
-        console.dir(this.countriesData);
       }
     )
 
-
-
-    if(window.localStorage.getItem('toggledTheme')){
+    if (window.localStorage.getItem('toggledTheme')) {
       this.changeTheme = true;
     }
-
 
   }
 
 
-  themeHandler(event: boolean){
+  themeHandler(event: boolean) {
     this.changeTheme = event;
   }
 
   ngOnDestroy(): void {
-      this.countryDataSub?.unsubscribe();
+    this.countryDataSub?.unsubscribe();
   }
 
   search(value: string): void {
-    this.countriesData.filter((val:any) =>
+    this.countriesData.filter((val: any) =>
       val?.name?.common?.toLowerCase().includes(value));
-      console.log('Trying to search');
   }
 
-  filterRegion(event: any){
-    console.log(event.target.value);
+
+  filterRegion(event: any) {
     this.selectedRegion = event.target.value;
 
-    if(this.selectedRegion === 'All'){
+    if (this.selectedRegion === 'All') {
       this.countriesDataCopy = this.countriesData;
     } else {
-
-    
-
-    this.countriesDataCopy = this.countriesData.filter((item: any) => item.region === this.selectedRegion);
-
-    console.log(this.countriesDataCopy);
+      this.countriesDataCopy = this.countriesData.filter((item: any) => item.region === this.selectedRegion);
     }
   }
 
