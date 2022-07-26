86aa67d88f2ecf24b8f1ce5a34be6d4175db0c6f
Mihai Mirzac <mihaimarzac@gmail.com>
2022-07-26 16:05:30 +0300

    Renamed param from name to country code

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CountryService } from 'src/app/services/country.service';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -17,70 +17,66 @@ export class CountryComponent implements OnInit, OnDestroy {
 
   neighbourCountriesSub?: Subscription;
 
-  firstLanguage?:any;
+  firstLanguage?: any;
   currency?: any;
   languages?: any;
-  borderCountries? : any;
+  borderCountries?: any;
 
   borderCountriesData?: any[] = [];
 
   changeTheme: boolean = false;
 
-  themeHandler(event: boolean){
+  themeHandler(event: boolean) {
     this.changeTheme = event;
   }
 
 
   ngOnInit(): void {
 
-    
-    if(window.localStorage.getItem('toggledTheme')){
+
+    if (window.localStorage.getItem('toggledTheme')) {
       this.changeTheme = true;
     }
 
     this.route.paramMap.subscribe(
-      params=>{
-        const name = params.get('name');
-       // console.log(name);
+      params => {
+        const code = params.get('code');
 
-        if(name){
-         this.singleCountryDataSub = this.countryService.getCountryByName(name).subscribe(
-            res=>{
+        if (code) {
+          this.singleCountryDataSub = this.countryService.getCountryByCode(code).subscribe(
+            res => {
               this.singleCountryData = res;
 
-              console.log(this.singleCountryData[0].cca3);
 
               this.firstLanguage = Object.keys(this.singleCountryData[0].languages)[0];
               this.currency = Object.entries(this.singleCountryData[0].currencies)[0];
               this.languages = Object.entries(this.singleCountryData[0].languages);
-      
 
               this.borderCountries = this.singleCountryData[0].borders;
 
-             this.neighbourCountriesSub = this.countryService?.getNeighboursByCode(this.borderCountries).subscribe(
-                resp=>{
-              this.borderCountriesData = resp;
 
-             // console.log(resp);
+              this.neighbourCountriesSub = this.countryService?.getNeighboursByCode(this.borderCountries).subscribe(
+                resp => {
+                  this.borderCountriesData = resp;
                 }
               )
-              //console.log(this.currency);
+
             }
           )
         }
 
       }
     )
-    
-
-
-}
 
 
 
-ngOnDestroy(): void{
-this.singleCountryDataSub?.unsubscribe();
-this.neighbourCountriesSub?.unsubscribe();
-}
+  }
+
+
+
+  ngOnDestroy(): void {
+    this.singleCountryDataSub?.unsubscribe();
+    this.neighbourCountriesSub?.unsubscribe();
+  }
 
 }
